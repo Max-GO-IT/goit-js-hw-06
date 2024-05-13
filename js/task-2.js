2eeaba042966737d7173d0bd632bfa260aa334b0
Max-GO-IT <terletskymaxim@ukr.net>
2024-05-13 23:27:21 +0300

    ...

--- js/task-2.js
+++ js/task-2.js
@@ -1,17 +1,23 @@
-const Storage = {
-    items: [],
+class Storage {
+
+  constructor(items){
+    this.items=items;
+  }
+        
     getItems() {
     return this.items
-    },
+    }
+
     addItem(newItem) {
     this.items.push(newItem)
-    },
+    }
+    
     removeItem(itemToRemove){
       const index = this.items.indexOf(itemToRemove);
         if (index !== -1) {
           this.items.splice(index, 1);
     }
-    },
+    }
     }
     
     const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
--- js/task-3.js
+++ js/task-3.js
@@ -1,24 +1,27 @@
-const initialValue = "";
 
-const StringBuilder = {
-  value: initialValue,
+class StringBuilder  {
+#value
+
+ constructor(initialValue){
+    this.#value=initialValue
+ }
 
   getValue() {
-    return this.value;
-  },
+    return this.#value;
+  }
 
   padEnd(str) {
-    this.value = this.value.concat(str);// this.value=this.value+str;
-  },//ab+cd=abcd
+    this.#value = this.#value.concat(str);// this.value=this.value+str;
+  }//ab+cd=abcd
 
   padStart(str) {
-    this.value = str.concat(this.value);// this.value=str+this.value;
-  },//cd+as=cdab
+    this.#value = str.concat(this.#value);// this.value=str+this.value;
+  }//cd+as=cdab
 
   padBoth(str) {
     this.padEnd(str);
     this.padStart(str);
-  },
+  }
 };
 
 const builder = new StringBuilder(".");
