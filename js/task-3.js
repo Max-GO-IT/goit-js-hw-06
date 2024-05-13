6e06d3747fd157a0bfefd689e8abc44d3969636e
Max-GO-IT <terletskymaxim@ukr.net>
2024-05-13 15:47:52 +0300

    ...

---
+++ js/task-3.js
@@ -0,0 +1,31 @@
+const initialValue = "";
+
+const StringBuilder = {
+  value: initialValue,
+
+  getValue() {
+    return this.value;
+  },
+
+  padEnd(str) {
+    this.value = this.value.concat(str);// this.value=this.value+str;
+  },//ab+cd=abcd
+
+  padStart(str) {
+    this.value = str.concat(this.value);// this.value=str+this.value;
+  },//cd+as=cdab
+
+  padBoth(str) {
+    this.padEnd(str);
+    this.padStart(str);
+  },
+};
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
